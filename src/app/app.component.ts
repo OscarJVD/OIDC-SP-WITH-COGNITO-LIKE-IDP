ae6a6d97ab30570f53d97fc8211639ed683f49dd
OscarJVD <ojvargas30@misena.edu.co>
2021-10-20 13:16:08 -0500

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { OAuthService, JwksValidationHandler } from 'angular-oauth2-oidc';
+import { authConfig } from './sso.config';
+import { filter } from 'minimatch';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+
+  constructor(private oauthService:OAuthService){
+    this.configureSingleSignOn();
+  }
+
+  configureSingleSignOn(){
+    this.oauthService.configure(authConfig);
+    this.oauthService.tokenValidationHandler = new JwksValidationHandler(); // tkn - id del idp
+    this.oauthService.loadDiscoveryDocumentAndTryLogin()
+      .then(res => { console.log(res); })
+      .catch(error => {console.log(error); });
+  }
+
+  login(){
+    this.oauthService.initImplicitFlow();
+  }
+
+  logout(){
+    this.oauthService.logOut();
+  }
+
+  get token(){
+    let claims:any = this.oauthService.getIdentityClaims();
+    return claims ? claims : null;
+  }
+}
